b404f21ac3a4cc8a2295af337d6a9b2389a02fd4
tongsq19 <tongsq14@mails.tsinghua.edu.cn>
2019-03-10 05:15:12 +0800

    Pass MyMap Test

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -17,22 +18,59 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        for(int i = 0; i < array.size(); ++i) {
+            resultList.add(3 * this.array.get(i));
+        }
+        return resultList;
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        List<String> resultList = new ArrayList<>();
+        for(int i = 0; i < array.size(); ++i) {
+            resultList.add(letterList.get(i));
+        }
+        return resultList;
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        List<String> resultList = new ArrayList<>();
+
+        for(int i = 0; i < array.size(); ++i) {
+            int value = array.get(i);
+            String letters;
+
+            if(value > 26) {
+                if(value%26 == 0) {
+                    letters = letterList.get(value/26 - 2) + letterList.get(value%26 + 25);
+                } else {
+                    letters = letterList.get(value/26 - 1) + letterList.get(value%26 - 1);
+                }
+
+            } else {
+                letters = letterList.get(value%26 - 1);
+            }
+            resultList.add(letters);
+        }
+        return resultList;
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        Integer[] arr= array.toArray(new Integer[array.size()]);
+        Arrays.sort(arr);
+        for(int i=arr.length-1; i >= 0; --i) {
+            resultList.add(arr[i]);
+        }
+
+        return resultList;
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        List<Integer> resultList = new ArrayList<>();
+        Integer[] arr= array.toArray(new Integer[array.size()]);
+        Arrays.sort(arr);
+        resultList.addAll(Arrays.asList(arr));
+        return resultList;
     }
 }
